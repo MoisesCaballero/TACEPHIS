805c6890e528ab5e99555d1426000302028a10de
Wilson Daniel Ospina <wdospinal@unal.edu.co>
2020-04-20 02:06:25 -0500

    add: translations

---
+++ src/translations/en.js
@@ -0,0 +1,16 @@
+const en = {
+  account: 'Account',
+  login: 'Login',
+  alarm: 'Alarm',
+  chat: 'Chat',
+  register: 'Register',
+  configuration: 'Configuration',
+  medic: 'Medic',
+  polls: 'Polls',
+  placeholderEmail: 'Your Email',
+  placeholderPassword: 'Your Password',
+  loginTitle: 'Please Login!',
+  fogotPassword: 'Forgot Password?',
+  resetPassword: 'Reset Password',
+};
+export default en;
