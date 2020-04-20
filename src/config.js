8df08f9fd71703206d8d1f23489623fdf3847318
Wilson Daniel Ospina <wdospinal@unal.edu.co>
2020-04-20 02:07:35 -0500

    add: config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const backgroundColor = '#FFF';
+export const primaryColor = '#2B303A';
+export const tabBarColor = '#2C2C2C';
+export const iconColor = '#2C2C2C';
+
+export const textColor = {
+  black: '#F2B900',
+  white: '#00CC99',
+  principal: 'tomato',
+};
